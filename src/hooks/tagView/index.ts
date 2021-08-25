c0541e696204c61a894ee4c1f91a1148227925cc
chenying <chenying@witsky.cn>
2021-08-25 15:00:11 +0800

    docs: 添加一些注释

--- src/hooks/tagView/index.ts
+++ src/hooks/tagView/index.ts
@@ -10,6 +10,14 @@ const tagView: ITagViewAction = {
 function useTagView() {
   const [tags, dispatch] = useReducer(tagViewReducer, tagView)
 
+  /*
+  * 这里其实可以用函数重载的方式去优化代码（function overload）
+  * 但是外部就需要记得有哪些类型，调用的时候传入action类型，且修改的时候需要修改多处文件
+  * 我这里这样的设计外部就不用关心类型有哪些，且如果发生变动，改这里一个文件即可
+  * 且，我认为业务代码命名应该是要表意的，具体的方法名让业务逻辑更清晰
+  * 且，万一他们以后各自会有别的业务场景呢
+  */
+
   const handleAddTag = (route: IRoute): void => {
     dispatch(addTag(route))
   }
--- vite.config.ts
+++ vite.config.ts
@@ -23,6 +23,10 @@ export default defineConfig({
   plugins: [
     reactRefresh(),
     gzipPlugin(),
+    /* 
+    * legacy的文件不知道咋配打包后的目录，目前他一个跑到assets文件夹下了，一个在js目录下，搞不懂
+    * legacy的文件没有gz文件啊，为啥啊，伤脑筋
+    */
     legacy({
       targets: [
         'last 1 version',
@@ -36,7 +40,9 @@ export default defineConfig({
         'last 3 iOS versions'
       ]
     })
+    // 自定义插件调用的方式
     // mocks(),
+    // 运行时的代码检测，但是无效，反正我提交的时候做了husky校验，这里不行就不行吧
     // {
     //   ...eslint({
     //     include: 'src/*.tsx',
@@ -54,6 +60,14 @@ export default defineConfig({
       }
     }
   },
+  /* 
+  * 其实这里只要全局注入后其他地方就可以不用手动注入了
+  * 但是ts检测里是会报错的。。为了不影响开发时的感官，就不用这种全局注入的模式了
+  * 挺可惜的，要是ts也能检测到全局注入的依赖就好了
+  */
+  // esbuild: {
+  //   jsxInject: `import React, { FC, useState, useEffect, useContext, useReducer } from 'react'`
+  // },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
